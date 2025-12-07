74db95fb02fd2941786c1dc86bef541cb2c3b122
ahmedhossam3313f-lang <ahmedhossam3313f@gmail.com>
2025-12-07 14:08:29 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { RadioProvider } from "@/lib/radio-context";
 import { AuthProvider } from "@/lib/auth-context";
